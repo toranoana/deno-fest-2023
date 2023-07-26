e7b988248e49ef439a8570252b37154245dea50a
奥谷 一陽 <1068@JRY7VYLFPP.local>
2023-07-26 20:46:20 +0900

    title を修正

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -6,7 +6,7 @@ export default function App({ Component}: AppProps) {
   return (
     <>
       <Head>
-        <title>test</title>
+        <title>Deno Fest ディノフェス - presented by toranoana.deno </title>
       </Head>
 
       <div className="wrapper overflow-x-hidden">
