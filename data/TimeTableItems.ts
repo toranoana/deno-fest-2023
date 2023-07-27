4d9c4fb76dae241946f39fd4cd79e99fdf8d468c
奥谷 一陽 <1068@JRY7VYLFPP.local>
2023-07-27 18:38:17 +0900

    タイムテーブル修正

--- data/TimeTableItems.ts
+++ data/TimeTableItems.ts
@@ -15,7 +15,7 @@ export const timeTableItems = [
   },
   {
     time: "13:10 ~ 13:35	",
-    title: "基調講演 deno ♥️ npm: Bridging the Gap Between Node and Deno",
+    title: "基調講演： deno ♥️ npm: Bridging the Gap Between Node and Deno",
     description:
       "Deno's arrival reinvented JavaScript runtime with its focus on security and TypeScript support. Yet, the difference in module systems with Node.js has been a challenge, especially with the vast npm packages left untapped for Deno. No more. We've introducing support for Node built-in modules and npm modules in Deno. This talk will detail the journey of implementing npm support, revealing the hurdles, solutions, and the remarkable possibilities it opens. It's our move to reconcile the power of the npm ecosystem with Deno's secure, efficient runtime.",
     speakerName: "Ryan Dahl - ライアン・ダール",
@@ -30,21 +30,21 @@ export const timeTableItems = [
   },
   {
     time: "13:55 ~ 14:20",
-    title: "JavaScript Server Runtime History",
+    title: "セッション 1：JavaScript Server Runtime History",
     description: "JavaScript Server Runtime の過去と現在と未来について",
     speakerName: "Yosuke Furukawa - 古川 陽介",
     speakerImage: "/images/speaker_01.jpeg",
   },
   {
     time: "14:20 ~ 14:35",
-    title: "開発チームに聞こうのコーナー1",
+    title: "セッション 2：開発チームに聞こうのコーナー1",
     description: "",
     speakerName: "Deno 開発チーム",
     speakerImage: "/images/img_deno.png",
   },
   {
     time: "14:35 ~ 14:50",
-    title: "coming soon",
+    title: "セッション 3：coming soon",
     description: "",
     speakerName: "",
     speakerImage: "",
@@ -58,7 +58,7 @@ export const timeTableItems = [
   },
   {
     time: "15:05 ~ 15:20",
-    title: "SupabaseのAI検索機能",
+    title: "セッション 4：SupabaseのAI検索機能",
     description:
       "SupabaseにはSupabase ClippyというAI検索機能があります。こちらはユーザーの検索に対してSupabase Edge functionsとOpenAIを駆使してChatGPT風に回答結果をユーザーに返すようになっているものです。最近だとMozillaさん同じ検索機能をサイトに追加しており、今後デファクトになるかもしれないAI検索機能！そんなAI検索機能がどのように実装されているのかをお話しいたします。",
     speakerName: "Tyler - タイラー",
@@ -66,14 +66,14 @@ export const timeTableItems = [
   },
   {
     time: "15:20 ~ 15:35",
-    title: "coming soon",
+    title: "セッション 5：coming soon",
     description: "",
     speakerName: "",
     speakerImage: "",
   },
   {
     time: "15:35 ~ 15:50",
-    title: "金曜 DENO DE SHOW",
+    title: "セッション 6：金曜 DENO DE SHOW",
     description:
       "一人の技術者が仕事を進めるために必要だった、ヤクの毛刈りに関するおはなし。非同期処理を抽象的に扱うため、unknown 型の扱いを楽にするため、Worker メッセージを抽象的に扱うため、などなど、開発してきた数多のサードパーティモジュールの開発動機や使い方紹介など。",
     speakerName: "Alisue - ありすえ",
@@ -88,28 +88,28 @@ export const timeTableItems = [
   },
   {
     time: "16:00 ~ 16:15",
-    title: "coming soon",
+    title: "セッション 7：coming soon",
     description: "",
     speakerName: "",
     speakerImage: "",
   },
   {
     time: "16:15 ~ 16:30",
-    title: "coming soon",
+    title: "セッション 8：coming soon",
     description: "",
     speakerName: "",
     speakerImage: "",
   },
   {
     time: "16:30 ~ 16:45",
-    title: "coming soon",
+    title: "セッション 9：coming soon",
     description: "",
     speakerName: "",
     speakerImage: "",
   },
   {
     time: "16:45 ~ 17:00",
-    title: "Civic Tech by Open ESM in Pure JavaScript",
+    title: "セッション 10：Civic Tech by Open ESM in Pure JavaScript",
     description:
       "36年、100のプログラミング言語を経験して辿り着いたベスト、それが ESM in Pure JavaScript on Deno。シビックテック集団、Code for FUKUIのESモジュールトップ5と共にその魅力をご紹介します。",
     speakerName: "Taisuke Fukuno - 福野泰介",
@@ -124,22 +124,22 @@ export const timeTableItems = [
   },
   {
     time: "17:10 ~ 17:20",
-    title: "Deno KV でログを永続化する",
+    title: "セッション 11：Deno KV でログを永続化する",
     description:
       "Deno KV ハッカソンに提出して技術賞を頂いたログ永続化ライブラリ&デモアプリの設計や制作過程で得られた知見を紹介します。",
     speakerName: "Shun Ueda (Chiezo) - 上田 駿",
-    speakerImage: "/images/img_deno.png",
+    speakerImage: "/images/speaker_05.png",
   },
   {
     time: "17:20 ~ 17:30",
-    title: "coming soon",
+    title: "セッション 12：coming soon",
     description: "",
     speakerName: "",
     speakerImage: "",
   },
   {
     time: "17:30 ~ 17:40",
-    title: "coming soon",
+    title: "セッション 13：coming soon",
     description: "",
     speakerName: "",
     speakerImage: "",
@@ -153,28 +153,28 @@ export const timeTableItems = [
   },
   {
     time: "17:50 ~ 18:00",
-    title: "coming soon",
+    title: "セッション 14：coming soon",
     description: "",
     speakerName: "",
     speakerImage: "",
   },
   {
     time: "18:00 ~ 18:10",
-    title: "coming soon",
+    title: "セッション 15：coming soon",
     description: "",
     speakerName: "",
     speakerImage: "",
   },
   {
     time: "18:10 ~ 18:20",
-    title: "coming soon",
+    title: "セッション 16：coming soon",
     description: "",
     speakerName: "",
     speakerImage: "",
   },
   {
     time: "18:20 ~ 18:35",
-    title: "開発チームに聞こうのコーナー2",
+    title: "セッション 17：開発チームに聞こうのコーナー2",
     description: "",
     speakerName: "Deno 開発チーム",
     speakerImage: "/images/img_deno.png",
