6fd978736f71de7fa62776011010435fb6fe31ed
奥谷 一陽 <1068@JRY7VYLFPP.local>
2023-09-28 10:19:04 +0900

    会場住所の表記更新/行き方の表現を変更

--- routes/index.tsx
+++ routes/index.tsx
@@ -259,7 +259,7 @@ export default function Home() {
               <iframe className="h-[200px] md:h-[350px] rounded-lg" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2365.385951915328!2d139.7697966022491!3d35.69988444635659!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188c1c52026005%3A0x4e55145da9c5a233!2z44CSMTAxLTAwMjEg5p2x5Lqs6YO95Y2D5Luj55Sw5Yy65aSW56We55Sw77yR5LiB55uu77yX4oiS77yV!5e0!3m2!1sja!2sjp!4v1695640475761!5m2!1sja!2sjp" width="100%" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
               <div className="text-sm mt-4 flex flex-col sm:gap-2 sm:flex-row justify-between">
                 <p>
-                  東京都千代田区外神田1-7-5　フロントプレイス秋葉原 2階・3階<br />
+                  東京都千代田区外神田1-7-5　フロントプレイス秋葉原<br />
                   TKPガーデンシティPREMIUM秋葉原 3B
                 </p>
                 <a className="underline hover:no-underline" target="_blank" href="https://maps.app.goo.gl/HCNbiVEi8dzGgbiB6">Google mapでみる</a>
@@ -267,7 +267,7 @@ export default function Home() {
               {/* <div className="mt-12">
                 <h2 className="font-extrabold text-xl leading-none text-center mb-6">
                   Way<br />
-                  <span className="text-xs">行き方</span>
+                  <span className="text-xs">秋葉原駅からの道順</span>
                 </h2>
                 <iframe className="mx-auto rounded-lg h-[200px] md:h-[400px] max-w-full" width="700" height="400" src="https://www.youtube.com/embed/tnn3D8kPP_o?si=vakO61fkLJxuGYza" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
               </div> */}
